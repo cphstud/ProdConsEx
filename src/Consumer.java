9262f63b9d50d02c27413c48290f56099040e522
thor <thor@cphbusiness.dk>
2021-02-26 08:46:07 +0100

    consumer

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,20 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 class Consumer implements Runnable {
-    BlockingQueue<String> queue;
+    private BlockingQueue<Integer> inputFromFibbQueue;
+    private int sumQueue;
+    //private List<Integer> sumList;
+    private ArrayBlockingQueue<Integer> sumList;
 
-    public Consumer(BlockingQueue<String> queue) {
-        this.queue = queue;
+    public Consumer(BlockingQueue<Integer> input) {
+        inputFromFibbQueue = input;
+        sumList = new ArrayBlockingQueue<>(8);
+    }
+
+    private void printSum() throws InterruptedException {
+        int totalSum=0;
+        System.out.println(Thread.currentThread().getName() + " in printer" );
+        for (int i : sumList) {
+            System.out.println(Thread.currentThread().getName() + " in loop" + i );
+           totalSum +=sumList.take();
+        }
+        System.out.println("Sum: " + totalSum);
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                Thread.sleep(1000);
-                String msg = queue.take();
-
-                System.out.println("Consumed: " + msg);
+                int fibNum = inputFromFibbQueue.take();
+                System.out.println("Consumed: " + fibNum);
+                sumList.add(fibNum);
+                printSum();
             } catch (InterruptedException e) {
                 break;
             }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -41,16 +42,29 @@ class Producer implements Runnable{
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        BlockingQueue<Integer> fibbNums = new ArrayBlockingQueue<>(8);
-        BlockingQueue<Integer> numsForFibb = new ArrayBlockingQueue<>(8);
-        List<Integer> nums = List.of(4,5,8,12,21,22,34,35);
+        BlockingQueue<Integer> fibbNums = new ArrayBlockingQueue<>(15);
+        BlockingQueue<Integer> numsForFibb = new ArrayBlockingQueue<>(15);
+        List<Integer> nums = List.of(4,5,8,12,21,22,34,35,36,37,38,39,42,43);
         numsForFibb.addAll(nums);
         Iterator<Integer> it = numsForFibb.iterator();
         while(it.hasNext()) {
             System.out.println(it.next());
         }
-        Thread t = new Thread(new Producer(numsForFibb,fibbNums));
-        t.start();
+        Thread p1 = new Thread(new Producer(numsForFibb,fibbNums));
+        Thread p2 = new Thread(new Producer(numsForFibb,fibbNums));
+        Thread p3 = new Thread(new Producer(numsForFibb,fibbNums));
+        Thread p4 = new Thread(new Producer(numsForFibb,fibbNums));
+        Thread c = new Thread(new Consumer(fibbNums));
+        p1.setName("Producer1");
+        p2.setName("Producer2");
+        p3.setName("Producer3");
+        p4.setName("Producer4");
+        c.setName("Consumer");
+        p1.start();
+        p2.start();
+        p3.start();
+        p4.start();
+        c.start();
 
 
         /*
